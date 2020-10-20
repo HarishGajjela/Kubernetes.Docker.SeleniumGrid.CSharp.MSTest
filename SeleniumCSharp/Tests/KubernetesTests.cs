3aa89ac9bc2cf43b04ae41ee3b8188cfec9e4223
Harish Gajjela <gajjela43@gmail.com>
2020-10-20 21:43:12 +0530

    Enabled Parallerd & added Specs for Max Instances/Sessions

--- SeleniumCSharp/Tests/KubernetesTests.cs
+++ SeleniumCSharp/Tests/KubernetesTests.cs
@@ -5,19 +5,18 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
 using System;
 
-//[assembly: Parallelize(Workers = 3, Scope = ExecutionScope.MethodLevel)]
+[assembly: Parallelize(Workers = 5, Scope = ExecutionScope.MethodLevel)]
 namespace SeleniumCSharp
 {
     [TestClass]
     public class KubernetesTests
     {
         IWebDriver driver;
-        public string HubUrl = "127.0.0.1:51639";
+        public string HubUrl = "127.0.0.1:55758";
         [TestMethod]
         public void RemoteChromeTest1()
         {
             ChromeOptions options = new ChromeOptions();
-            options.AddArguments("disable-infobars");
             driver = new RemoteWebDriver(new Uri("http://" + HubUrl + "/wd/hub"), options);
             driver.Url="https://opensource-demo.orangehrmlive.com/";
             Console.WriteLine("title :" + driver.Title);
@@ -27,7 +26,6 @@ namespace SeleniumCSharp
         public void RemoteChromeTest2()
         {
             ChromeOptions options = new ChromeOptions();
-            options.AddArguments("disable-infobars");
             driver = new RemoteWebDriver(new Uri("http://" + HubUrl + "/wd/hub"), options);
             driver.Url = "https://www.linkedin.com/";
             Console.WriteLine("title :" + driver.Title);
@@ -37,9 +35,26 @@ namespace SeleniumCSharp
         public void RemoteChromeTest3()
         {
             ChromeOptions options = new ChromeOptions();
-            options.AddArguments("disable-infobars");
             driver = new RemoteWebDriver(new Uri("http://" + HubUrl + "/wd/hub"), options);
             driver.Url = "https://www.flipkart.com/";
+            Console.WriteLine("title :" + driver.Title);
+            driver.Quit();
+        }
+        [TestMethod]
+        public void RemoteChromeTest4()
+        {
+            ChromeOptions options = new ChromeOptions();
+            driver = new RemoteWebDriver(new Uri("http://" + HubUrl + "/wd/hub"), options);
+            driver.Url = "https://qascript.com/";
+            Console.WriteLine("title :" + driver.Title);
+            driver.Quit();
+        }
+        [TestMethod]
+        public void RemoteChromeTest5()
+        {
+            ChromeOptions options = new ChromeOptions();
+            driver = new RemoteWebDriver(new Uri("http://" + HubUrl + "/wd/hub"), options);
+            driver.Url = "https://learn-automation.com/";
             Console.WriteLine("title :" + driver.Title);
             driver.Quit();
         }
@@ -47,7 +62,6 @@ namespace SeleniumCSharp
         public void RemoteFirefoxTest4()
         {
             FirefoxOptions options = new FirefoxOptions();
-            options.AddArguments("disable-infobars");
             driver = new RemoteWebDriver(new Uri("http://" + HubUrl + "/wd/hub"), options);
             driver.Url = "https://www.youtube.com/";
             Console.WriteLine("title :" + driver.Title);
